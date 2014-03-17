1cd80f11f17e5a615b6ef500e996452847a55c9a
TomislavBlazeka <tomislav.blazeka@gmail.com>
2014-03-17 17:38:00 +0100

    Connected MainActivity with CategorySelect

--- CookBook/src/hr/svizec/cookbook/activities/MainActivity.java
+++ CookBook/src/hr/svizec/cookbook/activities/MainActivity.java
@@ -1,13 +1,15 @@
 package hr.svizec.cookbook.activities;
 
 import hr.svizec.cookbook.R;
-import hr.svizec.cookbook.R.layout;
-import hr.svizec.cookbook.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
+	
+	private String CR = "country";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,32 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void showAboutPage(View v)
+	{
+		
+	}
+	
+	public void showAmericanRecipes(View v)
+	{
+		Intent i = new Intent("hr.svizec.cookbook.CategorySelect");
+		i.putExtra(CR, "USA");
+		startActivity(i);
+	}
+	
+	public void showItalianRecipes(View v)
+	{
+		Intent i = new Intent("hr.svizec.cookbook.CategorySelect");
+		i.putExtra(CR, "Italy");
+		startActivity(i);
+	}
+	
+	public void showIndianRecipes(View v)
+	{
+		Intent i = new Intent("hr.svizec.cookbook.CategorySelect");
+		i.putExtra(CR, "India");
+		startActivity(i);
 	}
 
 }
