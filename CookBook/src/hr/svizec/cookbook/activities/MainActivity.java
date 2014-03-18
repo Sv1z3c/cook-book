cb82092b946a7ec3b5f7e57655e94898c4af8e56
TomislavBlazeka <tomislav.blazeka@gmail.com>
2014-03-18 19:53:20 +0100

    Added methods to preload a few recipes

--- CookBook/src/hr/svizec/cookbook/activities/MainActivity.java
+++ CookBook/src/hr/svizec/cookbook/activities/MainActivity.java
@@ -1,6 +1,14 @@
 package hr.svizec.cookbook.activities;
 
+import com.activeandroid.query.Select;
+
 import hr.svizec.cookbook.R;
+import hr.svizec.cookbook.data.Appetizer;
+import hr.svizec.cookbook.data.Dessert;
+import hr.svizec.cookbook.data.Drink;
+import hr.svizec.cookbook.data.MainDish;
+import hr.svizec.cookbook.data.Salad;
+import hr.svizec.cookbook.data.Soup;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -15,6 +23,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		addDefaultRecipes();
 	}
 
 	@Override
@@ -48,6 +58,35 @@ public class MainActivity extends Activity {
 		Intent i = new Intent("hr.svizec.cookbook.CategorySelect");
 		i.putExtra(CR, "India");
 		startActivity(i);
+	}
+	
+	private void initDatabase() {
+		int recipeCount = 0;
+		
+		recipeCount += new Select().from(MainDish.class).execute().size();
+		recipeCount += new Select().from(Appetizer.class).execute().size();
+		recipeCount += new Select().from(Dessert.class).execute().size();
+		recipeCount += new Select().from(Drink.class).execute().size();
+		recipeCount += new Select().from(Salad.class).execute().size();
+		recipeCount += new Select().from(Soup.class).execute().size();
+		
+		if(recipeCount == 0)
+		{
+			addDefaultRecipes();
+		}
+	}
+
+	private void addDefaultRecipes() {
+		MainDish ddl = new MainDish();
+		ddl.setCountry("USA");
+		ddl.setName("Deep Dish Lasagna");
+		ddl.setIngredients("12 uncooked lasagna noodles;" +
+				"1 pound sweet Italian sausage");
+		ddl.setPreparation("Preheat oven to 350 degrees F (175 degrees C). Lightly grease a 9x13-inch baking dish." +
+							"Bring a large pot of lightly salted water to a boil. Add " +
+							"lasagna noodles and cook for 8 to 10 minutes or until al dente; drain.");
+		
+		ddl.save();
 	}
 
 }
